c690e0fa4103a1ad5d36e73d49ed6cfa66fd4335
FleekwoodMac <margaretjeanwalker@gmail.com>
2023-08-08 18:18:56 -0700

    new test files

--- test/process_form.php
+++ test/process_form.php
@@ -1,18 +1,32 @@
 <?php
+require 'path_to_phpmailer/PHPMailerAutoload.php'; // Adjust the path to PHPMailer
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $recipient = "mjwalkerprojets@google.com"; // Replace with your email address
-    $subject = "New Contact Form Submission";
+    $recipient = "your@email.com"; // Replace with your email address
 
     $name = $_POST["name"];
     $email = $_POST["email"];
     $message = $_POST["message"];
 
-    $headers = "From: $email";
+    $mail = new PHPMailer;
+    
+    $mail->isSMTP();
+    $mail->Host = 'smtp.example.com'; // Replace with your SMTP server
+    $mail->SMTPAuth = true;
+    $mail->Username = 'your_username'; // Replace with your SMTP username
+    $mail->Password = 'your_password'; // Replace with your SMTP password
+    $mail->SMTPSecure = 'tls'; // Use 'ssl' if needed
+    $mail->Port = 587; // SMTP port
 
-    if (mail($recipient, $subject, $message, $headers)) {
+    $mail->setFrom($email, $name);
+    $mail->addAddress($recipient);
+    $mail->Subject = 'New Contact Form Submission';
+    $mail->Body = $message;
+
+    if ($mail->send()) {
         echo "Thank you for your message!";
     } else {
-        echo "Sorry, there was an error sending your message.";
+        echo "Sorry, there was an error sending your message. Error: " . $mail->ErrorInfo;
     }
 }
 ?>
---
+++ test/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'fleekwood/test',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '297bed7e01ac879ade92f492dd5036b774188259',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'fleekwood/test' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '297bed7e01ac879ade92f492dd5036b774188259',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.8.0',
+            'version' => '6.8.0.0',
+            'reference' => 'df16b615e371d81fb79e506277faea67a1be18f1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
