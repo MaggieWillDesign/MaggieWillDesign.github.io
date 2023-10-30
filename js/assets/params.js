680f03364697da36a2b1afac3a0d7503e1c61039
mjwalkerprojects <margaretjeanwalker@gmail.com>
2023-10-30 12:13:27 -0700

    updates

--- js/assets/params.js
+++ js/assets/params.js
@@ -1,23 +1,4 @@
-// function getParameterByName(name, url) {
-//     if (!url) {
-//         url = window.location.href;
-//     }
-//     name = name.replace(/[\[\]]/g, '\\$&');
-//     var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-//         results = regex.exec(url);
-//     if (!results) return null;
-//     if (!results[2]) return '';
-//     return decodeURIComponent(results[2].replace(/\+/g, ' '));
-// }
-
-
-// function updateTextElement() {
-//     var helloValue = getParameterByName('hello');
-//     var textElement = document.getElementById('textElement');
-//     if (helloValue) {
-//         textElement.textContent = helloValue + " your password is happydev";
-//     }
-// }
+// sets Params 
 
 function getParametersByName(names, url) {
     if (!url) {
@@ -44,33 +25,111 @@ function getParametersByName(names, url) {
 }
 
 function updateTextElement() {
-    var parameterNames = ['hello', 'hi', 'pw', 'source']; // Add more parameter names as needed
+    var parameterNames = ['hello', 'hi',]; // Add more parameter names as needed
     var parameterValues = getParametersByName(parameterNames);
-
     var textElement = document.getElementById('textElement');
+    var message = "";
 
-    // senior design
-    if (parameterValues['hi']) {
-        textElement.textContent = parameterValues['hi'] + " your password is 217";
-    }
+    // Use a switch statement to handle different parameter values
+    switch (true) {
+     
+        case parameterValues['hi'] === 'ARUP':
+            message = "ARUP your password is 217";
+            break;
 
-    //art director
-    if (parameterValues['pw']) {
-        textElement.textContent = parameterValues['pw'] + " your password is 159";
-    }
+        case parameterValues['hi'] === 'Capital Group':
+            message = "Capital Group your password is 217";
+            break;
 
-    // product design
-    if (parameterValues['hello']) {
-        textElement.textContent = parameterValues['hello'] + " your password is 281";
-    }
+        //use double quote mark due to apostrophie 
+        case parameterValues['hi'] === "GT's":
+            message = "GT's your password is 217";
+            break;
+
+
+        case parameterValues['hi'] === 'JJM':
+            message = "JJM your password is 217";
+            break;
+
+        case parameterValues['hi'] === 'Live Nation':
+            message = "Live Nation your password is 217";
+            break;
     
-    // linked in guest
-    if (parameterValues['source']) {
-        textElement.textContent = parameterValues['source'] + " get your password below";
-       
+        case parameterValues['hello'] === 'Vivian':
+            message = "";
+            break;
+
+        case parameterValues['hello'] === 'M13':
+            message = "M13 your password is 141";
+            break;
+        
     }
 
-   
-
-    // You can similarly handle other parameters like 'anotherparam' here.
+    textElement.textContent = message;
 }
+
+// end sets Params 
+
+// sets PW
+
+document.addEventListener("DOMContentLoaded", function () {
+    const passwordInput = document.getElementById("password-input");
+    const submitButton = document.getElementById("submit-button");
+    const contentDiv = document.getElementById("content");
+
+    submitButton.addEventListener("click", function () {
+        const password = passwordInput.value;
+        let redirectLink = "";
+
+        switch (password) {
+            // Group1: Goes to design professional vers all start 2
+
+            // round 1 apps will archive soon 
+            case "217":
+            // client label - M13  
+            case "141":
+            // client label    
+            case "152":
+
+                redirectLink = "pages/1/work.html";
+                break;
+
+            // Group2: Goes to AD ver
+
+            // client label   
+            case "2330":
+            // client label
+            case "2771":
+            // client label
+            case "2882":
+
+                redirectLink = "pages/ad1/work.html";
+                break;
+
+            // Group3: Goes to UX ver
+
+            // client label   
+            case "33300":
+            // client label
+            case "37711":
+            // client label
+            case "38822":
+
+                redirectLink = "pages/xd1/work.html";
+                break;
+
+            // Add more password cases as needed
+        }
+
+        if (redirectLink) {
+            // Redirect to the specific link
+            window.location.href = redirectLink;
+        } else {
+            // Handle the case where the password is not recognized
+            contentDiv.textContent = "Password not recognized. Please try again.";
+        }
+    });
+});
+
+
+// end sets PW
