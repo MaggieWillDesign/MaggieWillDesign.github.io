3c433024e737aa1f98df30097416881c3cbae8d6
mjwalkerprojects <margaretjeanwalker@gmail.com>
2023-11-02 18:15:14 -0700

    updates

--- js/assets/accordian.js
+++ js/assets/accordian.js
@@ -1,6 +1,12 @@
 $(document).ready(function () {
     $('.arrow').click(function () {
 
+        // Toggle the .icon-rotated class on the clicked Ionicon
+        $(this).find('.arrow-icon').toggleClass('icon-rotated');
+
+        // Toggle the wrapper margin
+        // $(this).find('.icon-wrapper').toggleClass('icon-rotated');
+
         $(".accordion__body").not($(this).next()).slideUp(400);
         $(this).next().slideToggle(400);
 
