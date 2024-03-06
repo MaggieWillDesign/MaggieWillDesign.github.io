7b4bcd29e10ef2d4d2f81c8bab38541114f06bbc
mjwalkerprojects <margaretjeanwalker@gmail.com>
2024-03-05 17:02:11 -0800

    updates

--- js/assets/vidsetting.js
+++ js/assets/vidsetting.js
@@ -20,3 +20,22 @@ const videoObserver = new IntersectionObserver((entries, observer) => {
 videoObserver.observe(video);
 
 
+// transparent video controls not working
+
+// const socialvideo = document.getElementById('socialVideo');
+// const customControls = document.createElement('div');
+// customControls.classList.add('custom-controls');
+
+// const playButton = document.createElement('button');
+// playButton.textContent = 'Play';
+// playButton.addEventListener('click', () => {
+//     if (video.paused) {
+//         video.play();
+//     } else {
+//         video.pause();
+//     }
+// });
+
+// customControls.appendChild(playButton);
+
+// video.parentNode.insertBefore(customControls, video.nextSibling);
